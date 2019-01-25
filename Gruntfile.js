597d20a2ab74b2fb13aa899b8f14aede3018baa7
Alexey Bass <alexey.bass@gmail.com>
2019-01-25 23:06:58 +0200

    Structure refactoring and season support in app
    Files moved to 'src' folder.
    Final app will be generated in 'build' folder.
    jQuery updated to 1.12.4 (was 1.11.1).
    Grunt reviewed and added result files summary.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,26 @@
-/* global module */
-
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        
+
         package: grunt.file.readJSON('package.json'),
-        
-        copyright: 
+
+        copyright:
             '/**\n' +
             ' * <%= package.name %> v<%= package.version %>\n' +
             ' * Copyright 2013-<%= grunt.template.today("yyyy") %> Alexey Bass.\n' +
             ' */\n',
-            
+
+        clean: ['build'],
+
+        mkdir: {
+            all: {
+                options: {
+                    mode: 0777,
+                    create: ['build']
+                },
+            },
+        },
+
         jshint: {
             options: {
                 laxcomma: true,
@@ -25,19 +34,7 @@ module.exports = function(grunt) {
                 }
             }
         },
-        
-        htmlmin: {
-            index: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'index.html': 'index.src.html'
-                }
-            }
-        },
-        
+
         jsonmin: {
             db: {
                 options: {
@@ -45,11 +42,11 @@ module.exports = function(grunt) {
                     stripComments: true
                 },
                 files: {
-                    'db.min.json': 'db.json'
+                    'build/db.min.json': 'src/db.json'
                 }
             }
         },
-        
+
         cssmin: {
             options: {
                 shorthandCompacting: false,
@@ -57,29 +54,29 @@ module.exports = function(grunt) {
             },
             target: {
                 files: {
-                    'css/combined.min.css': [
-                        'css/normalize-1.1.3.css',
-                        'css/main.css'
+                    'build/styles.min.css': [
+                        'src/css/normalize-1.1.3.css',
+                        'src/css/main.css'
                     ]
                 }
             }
         },
-        
+
         uglify: {
             options: {
                 preserveComments: 'some' // @preserve @license @cc_on
             },
             js: {
                 files: {
-                    'js/main.min.js': 'js/main.js'
+                    'build/scripts.min.js': 'src/js/scripts.js'
                 }
             }
         },
-        
+
         'string-replace': {
             index: {
                 files: {
-                    'main.src.html': 'main.src.html'
+                    'build/index.html': 'src/index.html'
                 },
                 options: {
                     replacements: [{
@@ -88,16 +85,57 @@ module.exports = function(grunt) {
                     }]
                 }
             }
+        },
+
+        htmlmin: {
+            index: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'build/index.html': 'build/index.html'
+                }
+            }
+        },
+
+        filesize: {
+            base: {
+                files: [
+                    {expand: true, cwd: 'build', src: ['*']}
+                ],
+                options: {
+                    ouput: [
+                        {
+                            stdout: true
+                        }
+                    ]
+                }
+            }
         }
     });
 
+    grunt.registerTask('test', 'Log some stuff.', function() {
+        grunt.log.write('Logging some stuff...').ok();
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-jsonmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-filesize');
 
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build',   ['string-replace', 'htmlmin', 'jsonmin', 'cssmin', 'uglify']);
+    // grunt.registerTask('build',   ['string-replace', 'htmlmin', 'jsonmin', 'cssmin', 'uglify']);
+    grunt.registerTask('build',   [
+        'clean', 'mkdir',
+        'string-replace', 'htmlmin',
+        'cssmin', 'uglify',
+        'jsonmin',
+        'filesize'
+    ]);
 };
